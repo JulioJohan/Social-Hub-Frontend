01b4aabc17c375d1e73c3e7deff937a10e2a947b
Alejandro Guerrero <92689016+alejandro026@users.noreply.github.com>
2023-07-05 15:40:41 -0600

    Mapeo de servicios de publicaciones

---
+++ src/app/models/post.modelDTO.ts
@@ -0,0 +1,9 @@
+export class PostDTO {
+    idPost:number;
+    description:number;
+    numLike:number;
+    multimedia:number;
+    share:number;
+    user:number;
+    multipartFile:any;
+}
---
+++ src/app/service/publicaciones.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Response } from "../models/response.model";
+import { Post } from "../models/post.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PublicacioneServices {
+  private urlAdmin = "http://localhost:8081/post";
+  private httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
+
+  private httpOptionsFormData = {
+    headers: new HttpHeaders({"enctype": "multipart/form-data" }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  public findAllPost(): Observable<Response<Post>> {
+    return this.http.get<Response<Post>>(`${this.urlAdmin}/findAllPost`, this.httpOptions)
+  }
+  public findByIdPost(idPost:number): Observable<Response<Post>> {
+    return this.http.get<Response<Post>>(`${this.urlAdmin}/findByIdPost/${idPost}`, this.httpOptions)
+  }
+  public findByUserPost(idUsuario:number): Observable<Response<Post>> {
+    return this.http.get<Response<Post>>(`${this.urlAdmin}/findByUserPost/${idUsuario}`, this.httpOptions)
+  }
+
+ public createPost(tipePost:number): Observable<Response<Post>> {
+    return this.http.post<Response<Post>>(`${this.urlAdmin}/createPost/${tipePost}`, {}, this.httpOptions)
+ }
+
+ public updatePost(): Observable<Response<Post>> {
+    return this.http.put<Response<Post>>(`${this.urlAdmin}/updatePost`, {}, this.httpOptions)
+ }
+
+ public deletePost(idPost:number): Observable<Response<Post>> {
+    return this.http.delete<Response<Post>>(`${this.urlAdmin}/deletePost/${idPost}`, this.httpOptions)
+ }
+
+ public likePost(idPost:number): Observable<Response<Post>> {
+    return this.http.post<Response<Post>>(`${this.urlAdmin}/likePost/${idPost}`, {}, this.httpOptions)
+ }
+ public unlikePost(idPost:number): Observable<Response<Post>> {
+    return this.http.post<Response<Post>>(`${this.urlAdmin}/unlikePost/${idPost}`, {}, this.httpOptions)
+ }
+}
